a7c582a25312645915962388cad4afaa363fd2a2
Ray <raypeaslee@gmail.com>
2021-02-13 18:03:16 -0500

    Update postServices.js
    styling issues

--- Server/routes/postServices.js
+++ Server/routes/postServices.js
@@ -3,23 +3,19 @@ const multer = require("multer")();
 
 module.exports = (app, auth) => {	
 
-	// //GET all posts the given user follows
-	// //returns a list of posts the user is following sorted by freshness
+	// GET all posts the given user follows
+	// returns a list of posts the user is following sorted by freshness
 	// app.get('/data/post', auth, PostController.getPostsForUser);
 
-	//POST new post
-	//creates a new post from given request.body object
+	// POST new post - creates a new post from given request.body object
 	app.post('/data/post', auth, multer.single('media'), PostController.create);
 
-	//PUT post
-	//updates a given post
+	// PUT post - updates a given post
 	app.put('/data/post/:id', auth, PostController.update);
 
-	//PUT a like for given posts
-	//updates given post and adds one like
+	// PUT a like for given posts - updates given post and adds one like
 	app.put('/data/post/:id/like', auth, PostController.addLike);
 
-	//DELETE post
-	//deletes a given user
+	// DELETE post - deletes a given user
 	app.delete('/data/post/:id', auth, PostController.remove);
 }
