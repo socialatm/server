f72e2276b4a9ca5e288a83b29a94b6a284c6139c
Ray <raypeaslee@gmail.com>
2021-02-13 19:37:06 -0500

    Update index.js
    tabs and spacing issue

--- index.js
+++ index.js
@@ -14,16 +14,16 @@ app.use(express.urlencoded({ extended: true })); //Parse URL-encoded bodies
 app.use(express.json()); //Used to parse JSON bodies
 
 if (process.env.NODE_ENV !== 'test'){
-    app.use(express.static(__dirname + '/Public'));
-    app.use(morgan('dev')); // log every request to the console
-    mongoose.connect('mongodb://localhost:27017/clonebookdb', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true});
-    mongoose.connection
-        .once('open', () => {
-            console.log('Connection to DB established');
-        })
-        .on('error', (error) => {
-            console.warn('Warning', error);
-        });
+  app.use(express.static(__dirname + '/Public'));
+  app.use(morgan('dev')); // log every request to the console
+  mongoose.connect('mongodb://localhost:27017/clonebookdb', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true});
+  mongoose.connection
+    .once('open', () => {
+      console.log('Connection to DB established');
+    })
+    .on('error', (error) => {
+      console.warn('Warning', error);
+    });
 }
 
 require('./Server/config/passport');
@@ -32,16 +32,16 @@ app.use(passport.initialize());
 router(app);
 
 app.get('/', function (req,res) {
-    // noinspection JSUnresolvedFunction
-    res.sendFile(__dirname + "/Public/index.html");
+  // noinspection JSUnresolvedFunction
+  res.sendFile(__dirname + "/Public/index.html");
 });
-// error handlers
-// Catch unauthorised errors
+
+// error handlers - Catch unauthorized errors
 app.use(function (err, req, res, next) {
-    if (err.name === 'UnauthorizedError') {
-        res.status(401);
-        res.json({"message" : err.name + ": " + err.message});
-    }
+  if (err.name === 'UnauthorizedError') {
+    res.status(401);
+    res.json({"message" : err.name + ": " + err.message});
+  }
 });
 
 module.exports = app;
