3462cfb9abea97719bf86cb2aa9bb0119e3cdbc4
Ray <raypeaslee@gmail.com>
2021-02-16 19:54:07 -0500

    finish removing mongoose-validator

--- api/comment/model/comment.js
+++ api/comment/model/comment.js
@@ -1,19 +1,10 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const validate = require('mongoose-validator');
-
-const contentValidator =
-    validate({
-        validator: 'isLength',
-        arguments: [1, 5000],
-        message: 'Content should be between {ARGS[0]} and {ARGS[1]} characters'
-    });
 
 const CommentSchema = new Schema({
     content: {
         type: String,
         required: [true, 'Content is required'],
-        validate: contentValidator
     },
     author: {
         type: Schema.Types.ObjectId,
