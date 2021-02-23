da9f1c390d5e38bf5e03f3670863a4c41986e8b4
Ray <raypeaslee@gmail.com>
2021-02-23 08:33:11 -0500

    Update friendship.js
    spacing issues

--- api/friendship/model/friendship.js
+++ api/friendship/model/friendship.js
@@ -2,36 +2,34 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const FriendshipSchema = new Schema({
-	userOne: {
-		type: Schema.Types.ObjectId,
-		required: [true, 'Requester required.'],
-		ref: 'user'
-	},
-	userTwo: {
-		type: Schema.Types.ObjectId,
-		required: [true, 'Target friend is required.'],
-		ref: 'user'
-	},
-	status: {
-		type: String,
-		enum: ['PENDING','APPROVED'],
-
-		default: 'PENDING'
-	}
+  userOne: {
+	type: Schema.Types.ObjectId,
+	required: [true, 'Requester required.'],
+	ref: 'user'
+  },
+  userTwo: {
+	type: Schema.Types.ObjectId,
+	required: [true, 'Target friend is required.'],
+	ref: 'user'
+  },
+  status: {
+	type: String,
+	enum: ['PENDING','APPROVED'],
+	default: 'PENDING'
+  }
 });
 
 FriendshipSchema.methods.approveFriendship = function () {
-	this.status = 'APPROVED'
-	return this.update();
+  this.status = 'APPROVED'
+  return this.update();
 };
 
 FriendshipSchema.methods.declineFriendship = function () {
-	return this.remove();
+  return this.remove();
 };
 
 FriendshipSchema.methods.getFriend = function (requester) {
-	if (requester.toString() === this.userOne.toString()) return this.userTwo;
-	return this.userOne;
+  if (requester.toString() === this.userOne.toString()) return this.userTwo;
+  return this.userOne;
 };
-
 module.exports = mongoose.model('friendship', FriendshipSchema);
