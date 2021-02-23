33f2308e3487e014b73172c9cdf17e084876478f
Ray <raypeaslee@gmail.com>
2021-02-23 08:14:19 -0500

    Update authentication.js
    remove a space. smallest commit ever

--- api/services/auth/authentication.js
+++ api/services/auth/authentication.js
@@ -4,7 +4,7 @@ const User = require(`${__basedir}/api/user/model/user.js`);
 
 module.exports.login = function (req, res) {
 
-  passport.authenticate ('local', function (err, user, info){
+  passport.authenticate('local', function (err, user, info){
     let token;
     // If Passport throws/catches an error
     if (err) {
