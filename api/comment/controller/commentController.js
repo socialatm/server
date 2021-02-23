97bf4e4f90416637c185b99577e01ba75352bb0a
Ray <raypeaslee@gmail.com>
2021-02-22 20:57:58 -0500

    Update commentController.js
    spacing & tab issues

--- api/comment/controller/commentController.js
+++ api/comment/controller/commentController.js
@@ -3,99 +3,100 @@ const Comment = require(`${__basedir}/api/comment/model/comment.js`);
 const User = require(`${__basedir}/api/user/model/user.js`);
 
 const create = (req, res) => {
-    const comment = new Comment({
-        content: req.body.content,
-        author: req.user._id
-    });
-    comment.save()
-        .then((commentFound) => {
-            Post.findByIdAndUpdate(req.params.postId, {
-                $addToSet: {
-                    "comments": commentFound._id
-                },
-                $set: {
-                    "updated": new Date().getTime()
-                }
-            }, {
-                new: true
-            })
-                .populate({
-                    path: 'comments',
-                    model: 'comment',
-                    populate: {
-                        path: 'author',
-                        model: 'user',
-                        select: 'username +_id + avatar'
-                    }
-                })
-                .then((post) => {
-                    const comments = post.comments;
-                    comments.sort((a, b) => {
-                        return b.updated < a.updated;
-                    });
-                    res.status(200).json(comments);
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
-        })
-        .catch((error) => {
-            res.status(500).json(error);
+  const comment = new Comment({
+    content: req.body.content,
+    author: req.user._id
+  });
+  comment.save()
+    .then((commentFound) => {
+      Post.findByIdAndUpdate(req.params.postId, {
+        $addToSet: {
+          "comments": commentFound._id
+        },
+        $set: {
+          "updated": new Date().getTime()
+        }
+      }, 
+      {
+        new: true
+      })
+      .populate({
+        path: 'comments',
+        model: 'comment',
+        populate: {
+          path: 'author',
+          model: 'user',
+          select: 'username +_id + avatar'
+        }
+      })
+      .then((post) => {
+        const comments = post.comments;
+        comments.sort((a, b) => {
+          return b.updated < a.updated;
         });
+          res.status(200).json(comments);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
 }
 
 const update = (req, res) => {
-    Comment.findByIdAndUpdate(req.params.id, req.body)
-        .then((post) => {
-            if (post) {
-                res.send('OK')
-            } else {
-                res.status(404).send('comment not found');
-            }
-        })
-        .catch((error) => {
-            res.json(error);
-        });
+  Comment.findByIdAndUpdate(req.params.id, req.body)
+    .then((post) => {
+      if (post) {
+        res.send('OK')
+      } else {
+        res.status(404).send('comment not found');
+      }
+    })
+    .catch((error) => {
+    res.json(error);
+    });
 }
 
 const remove = (req, res) => {
-    Comment.findByIdAndRemove(req.params.id)
-        .then(() => {
-            res.send('OK')
-        })
-        .catch((error) => {
-            res.status(500).json(error);
-        });
+  Comment.findByIdAndRemove(req.params.id)
+  .then(() => {
+    res.send('OK')
+  })
+  .catch((error) => {
+    res.status(500).json(error);
+  });
 }
 
 const addLike = (req, res) => {
-    Comment.findByIdAndUpdate(req.params.id, {
-        $addToSet: {
-            "likes": req.user._id
-        }
+  Comment.findByIdAndUpdate(req.params.id, {
+    $addToSet: {
+      "likes": req.user._id
+    }
+  })
+  .then(() => {
+    Post.findOne({
+      comments: req.params.id
     })
-        .then(() => {
-            Post.findOne({
-                comments: req.params.id
-            })
-                .then((post) => {
-                    if (post.author.toString() !== req.user._id) {
-                        User.findByIdAndUpdate(req.user._id, {
-                            $addToSet: {
-                                "followingPosts": req.params.id
-                            }
-                        })
-                            .then(() => {
-                                res.json('OK');
-                            });
-                    } else {
-                        res.json('OK');
-                    }
-                });
+    .then((post) => {
+      if (post.author.toString() !== req.user._id) {
+        User.findByIdAndUpdate(req.user._id, {
+          $addToSet: {
+            "followingPosts": req.params.id
+          }
         })
-        .catch((error) => {
-            res.status(500).json(error);
+        .then(() => {
+          res.json('OK');
         });
+      } else {
+        res.json('OK');
+      }
+    });
+  })
+  .catch((error) => {
+    res.status(500).json(error);
+  });
 }
 
 module.exports = {
