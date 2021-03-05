510b85031a80444cc5d130f3349b043a53198d49
Ray <raypeaslee@gmail.com>
2021-03-05 11:46:12 -0500

    Update test_helper.js
    formatting

--- test/test_helper.js
+++ test/test_helper.js
@@ -3,20 +3,20 @@ const mongoose = require('mongoose');
 before((done) => {
   mongoose.connect('mongodb://localhost:27017/testdb', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
   mongoose.connection
-    .once('open', () => { 
-    	console.log('Good To Go!');
-    	done(); 
+    .once('open', () => {
+      console.log('Good To Go!');
+      done();
     })
     .on('error', (error) => {
       console.warn('Warning', error);
     });
 });
 
-beforeEach((done) => {	
+beforeEach((done) => {
   const { users, comments, posts, friendships } = mongoose.connection.collections;
-  users.drop(() => {	
-    comments.drop(() => {	
-      posts.drop(() => {	
+  users.drop(() => {
+    comments.drop(() => {
+      posts.drop(() => {
         friendships.drop(() => {
           done();
         });
@@ -25,11 +25,11 @@ beforeEach((done) => {
   });
 });
 
-after((done) => {  
+after((done) => {
   const { users, comments, posts, friendships } = mongoose.connection.collections;
-  users.drop(() => {  
-    comments.drop(() => { 
-      posts.drop(() => {  
+  users.drop(() => {
+    comments.drop(() => {
+      posts.drop(() => {
         friendships.drop(() => {
           done();
         });
