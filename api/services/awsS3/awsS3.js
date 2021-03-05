8e0d58f3a7ff9faf3a7cddb750aa25923d7a12b1
Ray <raypeaslee@gmail.com>
2021-03-05 13:51:35 -0500

    Update awsS3.js
    formatting

--- api/services/awsS3/awsS3.js
+++ api/services/awsS3/awsS3.js
@@ -7,22 +7,22 @@ AWS.config.region = process.env.region;
 const s3 = new AWS.S3();
 
 const uploadS3 = (bucketName, file, contentType, key) => {
-    const uploadParams = {};
-    uploadParams.Body = file;
-    uploadParams.Key = key;
-    uploadParams.Bucket = bucketName;
-    uploadParams.ContenType = contentType;
-    return s3.upload(uploadParams).promise();
+  const uploadParams = {};
+  uploadParams.Body = file;
+  uploadParams.Key = key;
+  uploadParams.Bucket = bucketName;
+  uploadParams.ContenType = contentType;
+  return s3.upload(uploadParams).promise();
 };
 
 const deleteS3 = (bucketName, key) => {
-    const params = {};
-    params.Key = key;
-    params.Bucket = bucketName;
-    return s3.deleteObject(params).promise();
+  const params = {};
+  params.Key = key;
+  params.Bucket = bucketName;
+  return s3.deleteObject(params).promise();
 };
 
 module.exports = {
-    uploadS3,
-    deleteS3
+  uploadS3,
+  deleteS3
 };
