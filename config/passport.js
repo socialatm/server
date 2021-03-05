43cec2c03ea07cd7589adf709cd115184cb87813
Ray <raypeaslee@gmail.com>
2021-03-05 11:50:28 -0500

    Update passport.js
    formatting

--- config/passport.js
+++ config/passport.js
@@ -6,18 +6,18 @@ passport.use(new LocalStrategy(
   function (username, password, done) {
     User.findOne({ username: username })
       .select('+password')
-      .then((user) => {	
-      // Return if user not found in database
-	    if (!user) { return done(null, false); }
+      .then((user) => {
+        // Return if user not found in database
+        if (!user) { return done(null, false); }
         // Return if password is wrong
-        if (user.generateHash(password) !== user.password) { 
+        if (user.generateHash(password) !== user.password) {
           return done(null, false);
         }
         // If credentials are correct, return the user object
         return done(null, user);
-    	})
+      })
       .catch((error) => {
         return done(err);
-    	})
+      })
   }
 ));
