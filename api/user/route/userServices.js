28216c9905c9332b74797299d910fb14e1d4d465
Ray <raypeaslee@gmail.com>
2021-03-05 13:33:45 -0500

    Update userServices.js
    tabs

--- api/user/route/userServices.js
+++ api/user/route/userServices.js
@@ -3,30 +3,30 @@ const Authentication = require(`${__basedir}/api/services/auth/authentication.js
 const multer = require("multer")();
 
 module.exports = (app, auth) => {
-	// Register a new user - creates a new user from the given request.body object
-	app.post('/data/register', UserController.create);
+    // Register a new user - creates a new user from the given request.body object
+    app.post('/data/register', UserController.create);
 
-	// Login an existing user
-	app.post('/data/login', Authentication.login);
+    // Login an existing user
+    app.post('/data/login', Authentication.login);
 
-	// Get all users - returns a list of users filtered by given criteria
-	app.get('/data/user', auth, UserController.getUsers);
+    // Get all users - returns a list of users filtered by given criteria
+    app.get('/data/user', auth, UserController.getUsers);
 
-	// Get user by ID - returns one user wih given id
-	app.get('/data/user/:id', auth, UserController.getUser);
+    // Get user by ID - returns one user wih given id
+    app.get('/data/user/:id', auth, UserController.getUser);
 
-	// Get all posts of one user - returns all posts of given user
-	app.get('/data/user/:id/posts', auth, UserController.getPosts);
+    // Get all posts of one user - returns all posts of given user
+    app.get('/data/user/:id/posts', auth, UserController.getPosts);
 
-	// Get all friends for user - returns all friends of given user
-	app.get('/data/user/:id/friends', auth, UserController.getFriends);
+    // Get all friends for user - returns all friends of given user
+    app.get('/data/user/:id/friends', auth, UserController.getFriends);
 
-	// PUT a new image - add an profile avatar for given user
-	app.put('/data/user/img/:id', auth, multer.single("image"), UserController.addImage);
+    // PUT a new image - add an profile avatar for given user
+    app.put('/data/user/img/:id', auth, multer.single("image"), UserController.addImage);
 
-	// PUT user - updates a given user
-	app.put('/data/user/:id', auth, UserController.update);
+    // PUT user - updates a given user
+    app.put('/data/user/:id', auth, UserController.update);
 
-	// Delete user - deletes a given user
-	app.delete('/data/user/:id', auth, UserController.remove);
+    // Delete user - deletes a given user
+    app.delete('/data/user/:id', auth, UserController.remove);
 }
