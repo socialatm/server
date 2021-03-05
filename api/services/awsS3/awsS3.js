0164d0dee521b002482c02e7e25118ea5578fce0
Ray <raypeaslee@gmail.com>
2021-03-05 13:48:32 -0500

    Update awsS3.js
    change tabs to spaces

--- api/services/awsS3/awsS3.js
+++ api/services/awsS3/awsS3.js
@@ -8,21 +8,21 @@ const s3 = new AWS.S3();
 
 const uploadS3 = (bucketName, file, contentType, key) => {
     const uploadParams = {};
-	uploadParams.Body = file;
-	uploadParams.Key = key;
-	uploadParams.Bucket = bucketName;
-	uploadParams.ContenType = contentType;
-	return s3.upload(uploadParams).promise();
+    uploadParams.Body = file;
+    uploadParams.Key = key;
+    uploadParams.Bucket = bucketName;
+    uploadParams.ContenType = contentType;
+    return s3.upload(uploadParams).promise();
 };
 
 const deleteS3 = (bucketName, key) => {
-	const params = {};
-	params.Key = key;
-	params.Bucket = bucketName;
-	return s3.deleteObject(params).promise();
+    const params = {};
+    params.Key = key;
+    params.Bucket = bucketName;
+    return s3.deleteObject(params).promise();
 };
 
 module.exports = {
-	uploadS3,
-	deleteS3
+    uploadS3,
+    deleteS3
 };
