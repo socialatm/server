c41f2e109162a1b2563edf95d25d0a8b3e038142
Ray <raypeaslee@gmail.com>
2021-03-05 11:42:56 -0500

    Update friendshipServiceTests.js
    formatting

--- test/Service_tests/friendshipServiceTests.js
+++ test/Service_tests/friendshipServiceTests.js
@@ -4,9 +4,9 @@ const Friendship = require(`${__basedir}/api/friendship/model/friendship.js`);
 const request = require('supertest');
 const app = require(`${__basedir}/index.js`);
 
-describe('Testing Service methods for friendship', () => {	
-	let user , user2, user3, post1,post2,post3,friendship,token;
-	beforeEach((done) => {	
+describe('Testing Service methods for friendship', () => {
+	let user, user2, user3, post1, post2, post3, friendship, token;
+	beforeEach((done) => {
 		user = new User({
 			name: {
 				firstName: 'Joe',
@@ -68,27 +68,27 @@ describe('Testing Service methods for friendship', () => {
 			}
 		});
 		friendship = new Friendship({
-					userOne: user,
-					userTwo: user2
-				});
-		Promise.all([user.save(),user2.save(),user3.save(),friendship.save()])
-		.then(() => {			
-			request(app)
-				.post('/data/login')
-				.send({ username: 'alex', password: 'test123' })
-				.end((err,res) => {	
-					token = JSON.parse(res.text).token;
-					done();
-				})
-		});	
+			userOne: user,
+			userTwo: user2
+		});
+		Promise.all([user.save(), user2.save(), user3.save(), friendship.save()])
+			.then(() => {
+				request(app)
+					.post('/data/login')
+					.send({ username: 'alex', password: 'test123' })
+					.end((err, res) => {
+						token = JSON.parse(res.text).token;
+						done();
+					})
+			});
 	});
 
-	it('POST /data/friendship creates a new friends request', (done) => {	
+	it('POST /data/friendship creates a new friends request', (done) => {
 		request(app)
 			.post('/data/friendship')
 			.set('Authorization', 'Bearer ' + token)
 			.send({ id: user._id })
-			.end((err,response) => {
+			.end((err, response) => {
 				assert(response.body.status === 'PENDING');
 				assert(response.body.userOne.toString() === user3._id.toString());
 				assert(response.body.userTwo.toString() === user._id.toString());
@@ -97,29 +97,29 @@ describe('Testing Service methods for friendship', () => {
 			.post('/data/friendship')
 			.set('Authorization', 'Bearer ' + token)
 			.send({ id: user3._id })
-			.end((err,response) => {
+			.end((err, response) => {
 				assert(response.status === 400);
 				assert(response.text.includes("error"));
 				done();
 			});
 	});
 
-	it('PUT /data/friendship/:id updates the status of a friendship (declining and accepting)', (done) => {	
+	it('PUT /data/friendship/:id updates the status of a friendship (declining and accepting)', (done) => {
 		request(app)
 			.put(`/data/friendship/${friendship._id}`)
 			.set('Authorization', 'Bearer ' + token)
 			.send({ approved: true })
-			.end((err,response) => {
+			.end((err, response) => {
 				Friendship.findById(friendship._id)
-					.then((friends) => {	
+					.then((friends) => {
 						assert(friends.status === 'APPROVED');
 						request(app)
 							.put(`/data/friendship/${friendship._id}`)
 							.set('Authorization', 'Bearer ' + token)
 							.send({ declined: true })
-							.end((err,response) => {
+							.end((err, response) => {
 								Friendship.findById(friendship._id)
-									.then((friends) => {	
+									.then((friends) => {
 										assert(friends === null);
 										done();
 									})
